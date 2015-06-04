23ac1eec37e0314154daec500aec3606cc33d70c
Adam <sk82712@gmail.com>
2015-06-03 23:31:20 -0400

    Initial commit!

---
+++ README.md
@@ -0,0 +1,4 @@
+This is just a quick testing grounds for flask-materialize and a good starting poing for my own personal projects!   
+
+
+More to come. Sorry for the quick readme.
---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template  
+from flask_material import Material  
+from flask_wtf import Form, RecaptchaField
+from flask_wtf.file import FileField
+from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+    BooleanField, SubmitField, IntegerField, FormField, validators
+from wtforms.validators import Required
+
+app = Flask(__name__)  
+Material(app)  
+app.config['SECRET_KEY'] = 'USE-YOUR-OWN-SECRET-KEY-DAMNIT'
+app.config['RECAPTCHA_PUBLIC_KEY'] = 'TEST'
+
+# straight from the wtforms docs:
+class TelephoneForm(Form):
+    country_code = IntegerField('Country Code', [validators.required()])
+    area_code = IntegerField('Area Code/Exchange', [validators.required()])
+    number = TextField('Number')
+
+class ExampleForm(Form):
+    field1 = TextField('First Field', description='This is field one.')
+    field2 = TextField('Second Field', description='This is field two.',
+                       validators=[Required()])
+    hidden_field = HiddenField('You cannot see this', description='Nope')
+    recaptcha = RecaptchaField('A sample recaptcha field')
+    radio_field = RadioField('This is a radio field', choices=[
+        ('head_radio', 'Head radio'),
+        ('radio_76fm', "Radio '76 FM"),
+        ('lips_106', 'Lips 106'),
+        ('wctr', 'WCTR'),
+    ])
+    checkbox_field = BooleanField('This is a checkbox',
+                                  description='Checkboxes can be tricky.')
+
+    # subforms
+    mobile_phone = FormField(TelephoneForm)
+
+    # you can change the label as well
+    office_phone = FormField(TelephoneForm, label='Your office phone')
+
+    ff = FileField('Sample upload')
+
+    submit_button = SubmitField('Submit Form')
+
+
+    def validate_hidden_field(form, field):
+        raise ValidationError('Always wrong')
+
+@app.route('/')  
+def hello_world():
+      form = ExampleForm()   
+      return render_template('test.html', form = form)  
+
+if __name__ == '__main__':  
+    app.run(debug = True)  
